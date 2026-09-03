c1666ed80a6f7ec533e2620c81f3a7034c7e7106
mardhanawn <mardhanawn@gmail.com>
2026-09-03 15:10:34 +0700

    chore: initial project setup for elysia-coffeshop

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
+new Elysia()
+    .get('/user/:id', ({ params: { id } }) => id)
+    .listen(3000)
+
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
